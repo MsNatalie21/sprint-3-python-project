3290399ce35580cf40e56ecd81ca897caabe761d
MsNatalie21 <msjaney1986@gmail.com>
2026-06-10 21:17:52 -0400

    Update task1_variables.py
    a) Store the total number of games in the dataset in a variable called total_games. Print its value.
    
    b) Calculate the average global sales across all 20 games. Store the result in a variable called avg_global_sales and print it with a descriptive message.
    
    c) The game at index 0 (Wii Sports) has the highest global sales. Calculate what percentage of the total global sales (sum of all 20 games) it represents. Store the result in top_game_share and print it.

--- task1_variables.py
+++ task1_variables.py
@@ -1,1 +1,13 @@
+total_games = len(video_game_sales)
+print(total_games)
 
+total_global_sales = 0
+
+for game in video_game_sales:
+    total_global_sales += game[GLOBAL_SALES]
+
+avg_global_sales = total_global_sales / len(video_game_sales)
+print("Avg global sales:", avg_global_sales)
+
+top_game_share = (video_game_sales[0][GLOBAL_SALES] / total_global_sales) * 100
+print(top_game_share)
